11fb4e4359cae3a79f9a486ccdc7ed0bb0e14322
Lipskus <solornhunter1995@gmail.com>
2026-09-22 23:27:38 +0200

    Bump Sekaro UI version to 0.2.0

--- frontend/src/components/ui/Sidebar.jsx
+++ frontend/src/components/ui/Sidebar.jsx
@@ -32,7 +32,7 @@ const links = [
   { to: '/settings#general', key: 'settings', icon: <RiSettingsLine size={20} /> },
 ];
 
-const APP_VERSION = '0.1.1';
+const APP_VERSION = '0.2.0';
 
 function getOS() {
   const ua = navigator.userAgent;
