d47659f29b7b2b294bc3a37a395ff63716cef525
Lipskus <solornhunter1995@gmail.com>
2026-09-25 12:57:06 +0200

    feat(ui): load Sekaro design-token layer

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import './redesign/design.css';
+import './redesign/tokens.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
